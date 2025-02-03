6e4748ebce8b24c866b13c071259e08a109a75ce
devyacoub2000 <dev.yacoub@gmail.com>
2025-02-03 13:54:21 +0200

    db

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'type',
     ];
 
     /**
@@ -43,5 +44,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+     public function image() {
+        return $this->morphOne(image::class, 'imageable');
     }
 }
